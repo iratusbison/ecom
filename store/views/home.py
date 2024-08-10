3d16114f8404c3714b99694a4eb5dee2d61ee796
iratusbison <iratusbison@gmail.com>
2024-08-10 05:49:25 +0000

    ci some mess up clear

--- store/views/home.py
+++ store/views/home.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
 from store.models.product import Products
 from store.models.category import Category
 from django.views import View
-
 from django.http import JsonResponse
 
 class Index(View):
@@ -62,77 +60,6 @@ def store(request):
     return render(request, 'index.html', data)
 
 
-from django.shortcuts import render, get_object_or_404
-
 def product_detail(request, id):
     product = get_object_or_404(Products, id=id)
-=======
-from django.shortcuts import render, redirect, HttpResponseRedirect
-from store.models.product import Products
-from store.models.category import Category
-from django.views import View
-
-from django.http import JsonResponse
-
-class Index(View):
-    def post(self, request):
-        product = request.POST.get('product')
-        remove = request.POST.get('remove')
-        cart = request.session.get('cart')
-        if cart:
-            quantity = cart.get(product)
-            if quantity:
-                if remove:
-                    if quantity <= 1:
-                        cart.pop(product)
-                    else:
-                        cart[product] = quantity - 1
-                else:
-                    cart[product] = quantity + 1
-            else:
-                cart[product] = 1
-        else:
-            cart = {}
-            cart[product] = 1
-
-        request.session['cart'] = cart
-        # Prepare response data
-        new_quantity = cart.get(product, 0)
-        return JsonResponse({'success': True, 'new_quantity': new_quantity})
-
-    def get(self, request):
-        return HttpResponseRedirect(f'/store{request.get_full_path()[1:]}')
-
-
-def store(request):
-    cart = request.session.get('cart')
-    if not cart:
-        request.session['cart'] = {}
-
-    products = None
-    categories = Category.get_all_categories()
-    categoryID = request.GET.get('category')
-    query = request.GET.get('q')  # Get the search query from the request
-
-    if query:
-        products = Products.objects.filter(name__icontains=query)  # Search products by name
-    elif categoryID:
-        products = Products.get_all_products_by_categoryid(categoryID)
-    else:
-        products = Products.get_all_products()
-
-    data = {
-        'products': products,
-        'categories': categories
-    }
-
-    print('you are :', request.session.get('email'))
-    return render(request, 'index.html', data)
-
-
-from django.shortcuts import render, get_object_or_404
-
-def product_detail(request, id):
-    product = get_object_or_404(Products, id=id)
->>>>>>> origin/main
     return render(request, 'product_detail.html', {'product': product})
--- store/views/orders.py
+++ store/views/orders.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import check_password
 from store.models.customer import Customer
@@ -8,68 +7,36 @@ from store.models.orders import Order
 from store.middlewares.auth import auth_middleware
 
 class OrderView(View):
-
-
-    def get(self , request ):
+    def get(self, request):
         customer = request.session.get('customer')
         orders = Order.get_orders_by_customer(customer)
         total_price = Order.get_total_order_price(customer)
-         # Calculate reward points
+        # Calculate reward points
         reward_points = (total_price // 500) * 10
 
         # Update customer reward points
         try:
-            customer = Customer.objects.get(id=customer)
-            customer.reward_points += reward_points
-            customer.save()
+            customer_instance = Customer.objects.get(id=customer)
+            customer_instance.reward_points += reward_points
+            customer_instance.save()
         except Customer.DoesNotExist:
             # Handle case where customer does not exist
             pass
+
         print(orders)
-        return render(request , 'orders.html'  , {'orders' : orders, 'total_price': total_price,'reward_points' : reward_points})
-        
-    
-
+        return render(request, 'orders.html', {
+            'orders': orders,
+            'total_price': total_price,
+            'reward_points': reward_points
+        })
 
     def post(self, request):
-        order_id = request.POST.get('order_id')  
+        order_id = request.POST.get('order_id')
         try:
             order = Order.objects.get(id=order_id)
             order.cancelled = True  # Marking the order as cancelled
             order.save()
-            # Optionally, you might want to add a confirmation message or redirect
             return redirect('orders')  # Redirect to orders page or wherever appropriate
         except Order.DoesNotExist:
             # Handle case where order does not exist
-=======
-from django.shortcuts import render, redirect
-from django.contrib.auth.hashers import check_password
-from store.models.customer import Customer
-from django.views import View
-from store.models.product import Products
-from store.models.orders import Order
-from store.middlewares.auth import auth_middleware
-
-class OrderView(View):
-
-
-    def get(self , request ):
-        customer = request.session.get('customer')
-        orders = Order.get_orders_by_customer(customer)
-        print(orders,"i don't know")
-        return render(request , 'orders.html'  , {'orders' : orders})
-    
-
-
-    def post(self, request):
-        order_id = request.POST.get('order_id')  
-        try:
-            order = Order.objects.get(id=order_id)
-            order.cancelled = True  # Marking the order as cancelled
-            order.save()
-            # Optionally, you might want to add a confirmation message or redirect
-            return redirect('orders')  # Redirect to orders page or wherever appropriate
-        except Order.DoesNotExist:
-            # Handle case where order does not exist
->>>>>>> origin/main
             return redirect('orders')  # Redirect to orders page or handle error
