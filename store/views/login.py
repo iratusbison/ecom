f5e50a94e1cd127a9d423bb16d1f20bdb8e576ed
iratusbison <iratusbison@gmail.com>
2024-08-11 11:08:14 +0000

    CI

--- store/views/login.py
+++ store/views/login.py
@@ -1,38 +1,41 @@
-from django.shortcuts import render , redirect , HttpResponseRedirect
-from django.contrib.auth.hashers import  check_password
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth.hashers import check_password
 from store.models.customer import Customer
 from django.views import View
 
-
 class Login(View):
-    return_url = None
-
     def get(self, request):
-        Login.return_url = request.GET.get ('return_url')
-        return render (request, 'login.html')
+        return render(request, 'login.html')
 
     def post(self, request):
-        email = request.POST.get ('email')
-        password = request.POST.get ('password')
-        customer = Customer.get_customer_by_email (email)
+        postData = request.POST
+        email = postData.get('email')
+        password = postData.get('password')
+
+        # Validation
         error_message = None
-        if customer:
-            flag = check_password (password, customer.password)
-            if flag:
-                request.session['customer'] = customer.id
 
-                if Login.return_url:
-                    return HttpResponseRedirect (Login.return_url)
-                else:
-                    Login.return_url = None
-                    return redirect ('homepage')
-            else:
-                error_message = 'Invalid !!'
+        if not email or not password:
+            error_message = "Please fill out all fields."
         else:
-            error_message = 'Invalid !!'
+            try:
+                customer = Customer.objects.get(email=email)
+                # Compare the plain-text password
+                if password == customer.password:  # Compare plain-text password
+                    request.session['customer'] = customer.id
+                    return redirect('store')  # Redirect to the store page after successful login
+                else:
+                    error_message = "Invalid credentials. Please try again."
+            except Customer.DoesNotExist:
+                error_message = "Email not registered. Please sign up."
 
-        print (email, password)
-        return render (request, 'login.html', {'error': error_message})
+        data = {
+            'error': error_message,
+            'email': email
+        }
+        return render(request, 'login.html', data)
+
+
 
 def logout(request):
     request.session.clear()
